3e4c0ddae0f475495887a024e40cb3f5033b75e0
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-07-20 16:04:05 +0300

    Respect XDG base directory specification

--- cwitch/config.py
+++ cwitch/config.py
@@ -2,8 +2,16 @@
 from typing import Optional, TextIO
 from configparser import ConfigParser, NoOptionError, NoSectionError
 from pathlib import Path
+from os import environ
 
 from .__init__ import prog_name
+
+
+if environ.get("XDG_CONFIG_HOME"):
+    # The (or "") is to pass the type check.
+    xdg_config_home = Path(environ.get("XDG_CONFIG_HOME") or "")
+else:
+    xdg_config_home = Path.joinpath(Path.home(), ".config")
 
 
 def get_config(config_file: Optional[TextIO] = None) -> dict:
@@ -13,7 +21,7 @@ def get_config(config_file: Optional[TextIO] = None) -> dict:
     try:
         if config_file is None:
             config_file = open(
-                Path.joinpath(Path.home(), f".config/{prog_name}/config.ini"), "r"
+                Path.joinpath(xdg_config_home, prog_name, "config.ini"), "r"
             )
 
         config.read_file(config_file)
@@ -39,7 +47,7 @@ def get_following_channels(channels_file: Optional[TextIO] = None) -> tuple:
     try:
         if channels_file is None:
             channels_file = open(
-                Path.joinpath(Path.home(), ".config/cwitch/channels.ini"), "r"
+                Path.joinpath(xdg_config_home, prog_name, "channels.ini"), "r"
             )
 
         channels.read_file(channels_file)
